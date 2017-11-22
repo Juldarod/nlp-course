2880ec40f667f9ce43e3ed23fb2da49aa873ad02
Julián Rodríguez <juldarod10@gmail.com>
2017-11-21 23:25:43 -0500

    Restructuration

--- morphan/processing.py
+++ morphan/processing.py
@@ -3,103 +3,107 @@ from morphan.segmentation import segment
 from morphan.dictionary import make_dictionary
 from nltk.tokenize import TweetTokenizer
 
-emoticon = []
-money = []
-comma = []
-mayor = []
 
+class Processing:
+    emoticon = []
+    money = []
+    comma = []
+    mayor = []
 
-def get_dictionary():
-    return make_dictionary()
+    @staticmethod
+    def get_dictionary():
+        return make_dictionary()
 
+    @staticmethod
+    def nltk_tweet_tokenizer(phrase):
+        tokenizer = TweetTokenizer()
+        print('\nNLTK TweetTokenizer\n' + str(tokenizer.tokenize(phrase)) + '\n')
+        return tokenizer.tokenize(phrase)
 
-def nltk_tweet_tokenizer(phrase):
-    tokenizer = TweetTokenizer()
-    print('\nNLTK TweetTokenizer\n' + str(tokenizer.tokenize(phrase)) + '\n')
-    return tokenizer.tokenize(phrase)
+    @staticmethod
+    def my_tokenizer(tokenized_text, corpus):
+        new_tokenized_text = []
+        for token in tokenized_text:
+            if '#' in token or '@' in token:
+                print('Segmentando hashtag/nickname... ' + token)
+                segmented_token = segment(token, corpus)
+                print(segmented_token)
+                len_segmented_token = len(segmented_token)
+                for i in range(len_segmented_token):
+                    new_tokenized_text.append(segmented_token[i])
+            else:
+                new_tokenized_text.append(token)
+        print('\nTokenización con hashtags/nicknames \n' + str(new_tokenized_text) + '\n')
+        return new_tokenized_text
 
+    def get_raw_text(self, new_tokenized_text):
+        tokens = []
+        raw_text = ''
+        e = [':o', ':/', ':\'(', '>:o', '(:', ':)', '>.<', 'XD',
+             '-__-', 'o.O', ';D', ':-)', '@_@', ':P', '8D',
+             '>:(', ':D', '=|']
+        m = ['1']
+        c = ['\"', ')', ':']
+        g = ['>']
+        for token in new_tokenized_text:
+            if token in e:
+                self.emoticon.append([new_tokenized_text.index(token), token, 'E'])
+            elif token in m:
+                self.money.append([new_tokenized_text.index(token), token, '$'])
+            elif token in c:
+                self.comma.append([new_tokenized_text.index(token), token, ','])
+            elif token in g:
+                self.mayor.append([new_tokenized_text.index(token), token, 'G'])
+            else:
+                # if token not in tokens:
+                tokens.append(token)
+        for token in tokens:
+            raw_text += token + ' '
+        return raw_text.strip()
 
-def my_tokenizer(tokenized_text, corpus):
-    new_tokenized_text = []
-    for token in tokenized_text:
-        if '#' in token or '@' in token:
-            print('Segmentando hashtag/nickname... ' + token)
-            segmented_token = segment(token, corpus)
-            print(segmented_token)
-            len_segmented_token = len(segmented_token)
-            for i in range(len_segmented_token):
-                new_tokenized_text.append(segmented_token[i])
-        else:
-            new_tokenized_text.append(token)
-    print('\nTokenización con hashtags/nicknames \n' + str(new_tokenized_text) + '\n')
-    return new_tokenized_text
+    @staticmethod
+    def analyze_raw_text(raw_text):
+        return analyze(raw_text)
 
-
-def get_raw_text(new_tokenized_text):
-    tokens = []
-    raw_text = ''
-    e = [':o', ':/', ':\'(', '>:o', '(:', ':)', '>.<', 'XD',
-         '-__-', 'o.O', ';D', ':-)', '@_@', ':P', '8D',
-         '>:(', ':D', '=|']
-    m = ['1']
-    c = ['\"', ')', ':']
-    g = ['>']
-    for token in new_tokenized_text:
-        if token in e:
-            emoticon.append([new_tokenized_text.index(token), token, 'E'])
-        elif token in m:
-            money.append([new_tokenized_text.index(token), token, '$'])
-        elif token in c:
-            comma.append([new_tokenized_text.index(token), token, ','])
-        elif token in g:
-            mayor.append([new_tokenized_text.index(token), token, 'G'])
-        else:
-            # if token not in tokens:
-            tokens.append(token)
-    for token in tokens:
-        raw_text += token + ' '
-    return raw_text.strip()
-
-
-def analyze_raw_text(raw_text):
-    return analyze(raw_text)
-
-
-def make_text(analyzed_words, new_tokenized_text):
-    print(analyzed_words)
-    text = []
-    len_emoticon = len(emoticon)
-    len_money = len(money)
-    len_comma = len(comma)
-    len_mayor = len(mayor)
-    cont_emoticon = 0
-    cont_money = 0
-    cont_comma = 0
-    cont_mayor = 0
-    for token in new_tokenized_text:
-        if len_emoticon != 0 and token == emoticon[cont_emoticon][1]:
-            text.append([emoticon[cont_emoticon][1], '', emoticon[cont_emoticon][2], ''])
-            cont_emoticon += 1
-            len_emoticon -= 1
-        elif len_money != 0 and token == money[cont_money][1]:
-            text.append([money[cont_money][1], '', money[cont_money][2], ''])
-            cont_money += 1
-            len_money -= 1
-        elif len_comma != 0 and token == comma[cont_comma][1]:
-            text.append([comma[cont_comma][1], '', comma[cont_comma][2], ''])
-            cont_comma += 1
-            len_comma -= 1
-        elif len_mayor != 0 and token == mayor[cont_mayor][1]:
-            text.append([mayor[cont_mayor][1], '', mayor[cont_mayor][2], ''])
-            cont_mayor += 1
-            len_mayor -= 1
-        else:
-            for word in analyzed_words:
-                if token == word.split(' ')[0]:
-                    # print('')
-                    text.append(word.split(' '))
-    emoticon.clear()
-    money.clear()
-    comma.clear()
-    mayor.clear()
-    return text
+    def make_text(self, analyzed_words, new_tokenized_text):
+        print(analyzed_words)
+        text = []
+        emoticon = self.emoticon
+        money = self.money
+        comma = self.comma
+        mayor = self.mayor
+        len_emoticon = len(emoticon)
+        len_money = len(money)
+        len_comma = len(comma)
+        len_mayor = len(mayor)
+        cont_emoticon = 0
+        cont_money = 0
+        cont_comma = 0
+        cont_mayor = 0
+        for token in new_tokenized_text:
+            if len_emoticon != 0 and token == emoticon[cont_emoticon][1]:
+                text.append([emoticon[cont_emoticon][1], '', emoticon[cont_emoticon][2], ''])
+                cont_emoticon += 1
+                len_emoticon -= 1
+            elif len_money != 0 and token == money[cont_money][1]:
+                text.append([money[cont_money][1], '', money[cont_money][2], ''])
+                cont_money += 1
+                len_money -= 1
+            elif len_comma != 0 and token == comma[cont_comma][1]:
+                text.append([comma[cont_comma][1], '', comma[cont_comma][2], ''])
+                cont_comma += 1
+                len_comma -= 1
+            elif len_mayor != 0 and token == mayor[cont_mayor][1]:
+                text.append([mayor[cont_mayor][1], '', mayor[cont_mayor][2], ''])
+                cont_mayor += 1
+                len_mayor -= 1
+            else:
+                for word in analyzed_words:
+                    if token == word.split(' ')[0]:
+                        # print('')
+                        text.append(word.split(' '))
+        emoticon.clear()
+        money.clear()
+        comma.clear()
+        mayor.clear()
+        return text
--- morphan/views.py
+++ morphan/views.py
@@ -1,13 +1,14 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
 from morphan.forms import InputForm
-from morphan.processing import *
+from morphan.processing import Processing
 
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'morphan/home.html'
-    corpus = get_dictionary()
+    processing = Processing()
+    corpus = processing.get_dictionary()
 
     def get(self, request):
         form = InputForm()
@@ -15,11 +16,12 @@ class HomeView(TemplateView):
 
     def post(self, request):
         form = InputForm(request.POST)
+        processing = self.processing
         if form.is_valid():
-            tokenized_text = nltk_tweet_tokenizer(form.cleaned_data['tweet'])
-            new_tokenized_text = my_tokenizer(tokenized_text, self.corpus)
-            raw_text = get_raw_text(new_tokenized_text)
-            analyzed_words = analyze_raw_text(raw_text + '.')
-            text = make_text(analyzed_words, new_tokenized_text)
+            tokenized_text = processing.nltk_tweet_tokenizer(form.cleaned_data['tweet'])
+            new_tokenized_text = processing.my_tokenizer(tokenized_text, self.corpus)
+            raw_text = processing.get_raw_text(new_tokenized_text)
+            analyzed_words = processing.analyze_raw_text(raw_text + '.')
+            text = processing.make_text(analyzed_words, new_tokenized_text)
             args = {'form': form, 'text': text}
             return render(request, self.template_name, args)
